6771ebf2d17fc58fcbdc73fed73878fa1c48b04c
FERNANDO GALDINO DA SILVA FILHO <T01POR0117@unimedfortaleza.com.br>
2018-09-19 16:51:50 -0300

    first commit

---
+++ R/RegraQuantidade.R
@@ -0,0 +1,17 @@
+#' Predicao da regra de quantidade
+#'
+#' Essa funcao faz a predicao, de acordo com os paremetros, se certo atendimento e aprovado ou nao na regra de quantidade.
+#' @param codServMedHosp, qtdeServMedHospPermitida, qtdeServMedHosp, qtdeDiasUltimaSolicitacao.
+#' @return Aprovado ou Reprovado.
+#' @export
+#' @examples
+#' regraQuantidade(1010101, 2, 1, 5)
+regraQuantidade <- function(codServMedHosp, qtdeServMedHospPermitida, qtdeServMedHosp, qtdeDiasUltimaSolicitacao){
+    ### setwd("C:\\Users\\t01por0117\\Documents") ###
+    ### Get predictions ###
+    load("regra_quantidade.rda")	    
+    newdata5 <- data.frame(codServMedHosp = codServMedHosp, qtdeServMedHospPermitida = qtdeServMedHospPermitida, qtdeServMedHosp = qtdeServMedHosp, qtdeDiasUltimaSolicitacao = qtdeDiasUltimaSolicitacao)
+    pred5 <- predict(modelo, newdata5, type = "prob")
+    ### pred5[1,2] ###
+    return(ifelse (pred5[1,2] >= 0.95, "Aprovado","Nao aprovado"))
+}
