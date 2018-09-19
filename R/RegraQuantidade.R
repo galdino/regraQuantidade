22cd4c6a827fd7eb0119ef656e13b07f1e72e5b7
FERNANDO GALDINO DA SILVA FILHO <T01POR0117@unimedfortaleza.com.br>
2018-09-19 17:39:22 -0300

    RegraQuantidade alterado nova tentativa de ler .rda

--- R/RegraQuantidade.R
+++ R/RegraQuantidade.R
@@ -9,7 +9,7 @@
 regraQuantidade <- function(codServMedHosp, qtdeServMedHospPermitida, qtdeServMedHosp, qtdeDiasUltimaSolicitacao){
     ### setwd("C:\\Users\\t01por0117\\Documents") ###
     ### Get prediction ###
-    load(system.file("data/regra_quantidade.rda", package = "regraQuantidade"),output_dir = getwd(),intermediates_dir = getwd())
+    load(system.file(package = "regraQuantidade", "data", "regra_quantidade.rda"), output_dir = getwd(), intermediates_dir = getwd())
     newdata5 <- data.frame(codServMedHosp = codServMedHosp, qtdeServMedHospPermitida = qtdeServMedHospPermitida, qtdeServMedHosp = qtdeServMedHosp, qtdeDiasUltimaSolicitacao = qtdeDiasUltimaSolicitacao)
     pred5 <- predict(modelo, newdata5, type = "prob")
     ### pred5[1,2] ###
